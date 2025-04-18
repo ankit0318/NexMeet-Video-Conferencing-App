ad1a311715f38a8a930ea0fc11dbf5da7ee44699
ankit0318 <ankitsharma0318@gmail.com>
2025-04-18 22:15:43 +0530

    main

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,46 @@
-import Meeting from "@/components/MeetingPage"; // your client component
+/* -------------------------------------------------------------------------- */
+// *this is client componnent
+/* -------------------------------------------------------------------------- */
+"use client";
 
-export default function MeetingPage({ params }: { params: { id: string } }) {
-  return <Meeting id={params.id} />;
+
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import Loader from "@/components/ui/Loader";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+
+function Meeting() {
+  const { id } = useParams();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (typeof id !== "string") {
+    throw new Error("Invalid meeting ID");
+  }
+
+  const { call, isLoading } = useGetCallById(id);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      
+      <StreamCall call={call}>
+       
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </>
+  );
 }
+
+export default Meeting;
