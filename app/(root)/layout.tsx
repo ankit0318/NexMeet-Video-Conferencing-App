f6eeeb479335a1bfd1e1a5664f4ceab4c6bcf5f6
ankit0318 <ankitsharma0318@gmail.com>
2025-04-19 09:23:34 +0530

    main

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { StreamVideoProvider } from "@/provider/StreamVideoProvider";
+import StreamVideoProvider  from "@/provider/StreamVideoProvider";
 import React, { Suspense } from "react";
 import { Toaster } from "sonner";
 import Loader from "@/components/ui/Loader";
