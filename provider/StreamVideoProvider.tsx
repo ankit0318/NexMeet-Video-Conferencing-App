484ef77e8283058ceb9bd8df85c904c8590be89f
ankit0318 <ankitsharma0318@gmail.com>
2025-04-19 09:17:45 +0530

    main

--- provider/StreamVideoProvider.tsx
+++ provider/StreamVideoProvider.tsx
@@ -1,62 +1,36 @@
 "use client";
 import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import tokenProvider from "@/actions/stream.actions";
 import Loader from "@/components/ui/Loader";
 
 const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-export const StreamVideoProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const { user, isLoaded } = useUser();
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
   const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-
+  const { user, isLoaded } = useUser();
 
   useEffect(() => {
     if (!isLoaded || !user) return;
-    if (!API_KEY) {
-      console.error("Stream API key is missing");
-      return;
-    }
+    if (!API_KEY) throw new Error('Stream API key is missing');
 
-    const initClient = async () => {
-      try {
-       
-        const client = new StreamVideoClient({
-          apiKey: API_KEY,
-          user: {
-            id: user?.id,
-            name: user?.username || user?.id,
-            image: user?.imageUrl,
-          },
-          tokenProvider,
-          options: {},
-        });
+    const client = new StreamVideoClient({
+      apiKey: API_KEY,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
 
-        setVideoClient(client);
-      } catch (error) {
-        console.error("Error initializing Stream video client:", error);
-      } 
-    };
+    setVideoClient(client);
+  }, [user, isLoaded]);
 
-    initClient();
-
-    return () => {
-      videoClient?.disconnectUser();
-    };
-  }, [user?.id, isLoaded,user,videoClient]);
-
-  if (!videoClient) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        <Loader />
-      </div>
-    );
-  }
+  if (!videoClient) return <Loader />;
 
   return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
+
+export default StreamVideoProvider;
